b0c65110fbf10d27e7c68c24dbe5b9d211784eee
Afomia01 <150925057+Afomia01@users.noreply.github.com>
2024-02-23 14:34:49 +0300

    Add solution for Partition Array According to Given Pivot

---
+++ leetcode/partition-array-according-to-given-pivot.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        smaller=[]
+        larger=[]
+        equal=[]
+        for i in nums:
+            if i<pivot:
+                smaller.append(i)
+            elif i>pivot:
+                larger.append(i)
+            else:
+                equal.append(i)
+        return smaller+equal+larger
+
+        
